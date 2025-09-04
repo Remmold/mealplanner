e90c82cbb5b0a3cb94ab4a5a631623010096159b
Daniel Wahlgren <dwahlgren93@gmail.com>
2025-09-04 09:51:10 +0200

    Streamlit setup + foundation with navigation, sidebar and components

---
+++ pages/5_Preferences.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from ui.main.components import *
+from ui.sidebar.components import sidebar
+
+st.title("Preferences")
+sidebar()
---
+++ ui/sidebar/components.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+# Sidebar Components
+def sidebar():
+    with st.sidebar:
+        st.subheader("Filters")
+        st.text("Filters will go here later")
